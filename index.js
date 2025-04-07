3703f745bd9056861eadb9893f8af65f706378ba
debuthe12 <167316613+debuthe12@users.noreply.github.com>
2025-04-07 14:42:12 +0000

    delete

--- index.js
+++
@@ -1,9 +0,0 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-import AppNavigator from './src/navigation/AppNavigator';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => AppNavigator);
--- src/store/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import droneReducer from '@store/slices/droneSlice';
-
-export const store = configureStore({
-  reducer: {
-    drone: droneReducer,
-  },
-}); 
